bead1305200a8ce76e5267b77397fd870d57122a
Om Singh <omsingh1.tech@gmail.com>
2025-12-25 04:58:21 +0000

    DataTypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello")
+// we are using node js not browser
+ // code readability should be high
+console.log(3+3)
+console.log("Om")
+
+let name = "Om Singh" // string type
+let age = 18 // numtype
+let isLogged = false // boolean type
+
+// these are some primitive data type - 
+// number -> 2^53
+// bigim
+// string -> preffer in double code
+// boolean = true/false
+// null -> standalone value // khaali hai
+// undefined -> variable declare hai but value define nhi haoi
+// symbol -> unique
+
+// object - 
+
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+console.log(typeof "Om Singh")
